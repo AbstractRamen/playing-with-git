30fabc4b74aac355fb22e9b15f13a9ccad9ca1e4
Jasper Cheng <akita@Jindo.fios-router.home>
2018-12-25 14:47:38 -0500

    Set up for heroku

--- server/server.js
+++ server/server.js
@@ -28,6 +28,7 @@ var {User} = require('../models/users');
 // })
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 
@@ -70,8 +71,8 @@ app.get('/todo/:id', (req, res) => {
 
 })
 
-app.listen(3000, ()=> {
-  console.log("Started on port 3000");
+app.listen(port, ()=> {
+  console.log(`Started on port ${port}`);
 })
 
 
